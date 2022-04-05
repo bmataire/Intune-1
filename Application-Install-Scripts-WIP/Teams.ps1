7c6656363a8731bc9401ffeeceff798b0765c5ad
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2022-04-05 09:43:12 +0100

    deleted

--- Application-Install-Scripts-WIP/Teams.ps1
+++
@@ -1,4 +0,0 @@
-# Download the Application Name
-Write-Verbose "Downloading Microsoft Teams Machine-Wide Installer" -Verbose
-Invoke-WebRequest 'https://teams.microsoft.com/downloads/desktopurl?env=production&plat=windows&arch=x64&managedInstaller=true&download=true' -OutFile C:\temp\Teamsx64.msi
-Start-Process C:\Temp\Teamsx64.msi -ArgumentList "ALLUSERS=1 /qn /norestart"
