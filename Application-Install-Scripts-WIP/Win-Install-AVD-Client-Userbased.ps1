58146014c73550c748356f604c64902d0cad704f
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2022-04-25 15:38:33 +0100

    updated

---
+++ Application-Install-Scripts-WIP/Win-Install-AVD-Client-Userbased.ps1
@@ -0,0 +1,75 @@
+<# 
+.SYNOPSIS
+   Install Script for Azure Virtual Desktop (AVD/WVD) Latest Version.
+
+.DESCRIPTION
+   Downloads & Install Script for Azure Virtual Desktop (AVD/WVD) Latest Version.
+
+.EXAMPLE
+   PS C:\> .\Win-Install-AVD-Client.ps1
+   Save the file to your hard drive with a .PS1 extention and run the file from an elavated PowerShell prompt.
+
+.FUNCTIONALITY
+   PowerShell v1+
+#>
+
+Clear-Host
+Write-Verbose "Setting Arguments" -Verbose
+$StartDTM = (Get-Date)
+
+# Application Details.
+
+$Path = $env:TEMP;
+$Vendor = "Microsoft"
+$Product = "Azure Virtual Desktop Client"
+$PackageName = "RemoteDesktop"
+$Version = "lsr"
+$InstallerType = "msi"
+$Source = "$PackageName" + "." + "$InstallerType"
+$URL = "https://go.microsoft.com/fwlink/?linkid=2068602"
+$LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
+$UnattendedArgs = 'ALLUSERS="2" MSIINSTALLPERUSER="1" /qn'
+$ProgressPreference = 'SilentlyContinue'
+
+Start-Transcript $LogPS
+
+if( -Not (Test-Path -Path $PackageName ) )
+{
+    New-Item -ItemType directory -Path $PackageName
+}
+
+Set-Location $Path
+
+Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
+If (!(Test-Path -Path $Source)) {
+    Invoke-WebRequest -Uri $url -OutFile $Source
+         }
+        Else {
+            Write-Verbose "File exists. Skipping Download." -Verbose
+         }
+
+Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
+(Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+
+Write-Verbose "Customization" -Verbose
+
+## Pause for 45 seconds to allow install to completed, This can be adjusted as needed.
+Start-Sleep -Seconds 45
+
+## Cleanup Downloads
+Remove-Item $Path\$Source
+
+## Create Detection Method. 
+$logfilespath = "C:\logfiles"
+If(!(test-path $logfilespath))
+{
+      New-Item -ItemType Directory -Force -Path $logfilespath
+}
+
+New-Item -ItemType "file" -Path "c:\logfiles\$PackageName-$Version.txt"
+
+Write-Verbose "Stop logging" -Verbose
+$EndDTM = (Get-Date)
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalSeconds) Seconds" -Verbose
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalMinutes) Minutes" -Verbose
+Stop-Transcript
