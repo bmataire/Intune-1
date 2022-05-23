4772693dd674c83f78c45fec317695b8df3f30b2
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2022-05-23 10:27:49 +0100

    created

---
+++ Application-Install-Scripts/Get-ImageJ.ps1
@@ -0,0 +1,10 @@
+Start-Process "https://imagej.nih.gov/ij/download.html"
+
+## Create Detection Method. 
+$logfilespath = "C:\logfiles"
+If(!(test-path $logfilespath))
+{
+      New-Item -ItemType Directory -Force -Path $logfilespath
+}
+
+New-Item -ItemType "file" -Path "c:\logfiles\get-ImageJ.txt"
