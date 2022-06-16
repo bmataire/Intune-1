0bf708b8c40aa92546a4d78de7d113a196647837
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2022-06-16 10:15:09 +0100

    updated

---
+++ Evergreen-Application-Scripts-WIP/Update-Git-x86.ps1
@@ -0,0 +1,20 @@
+
+# get latest download url for git-for-windows 32-bit exe
+$git_url = "https://api.github.com/repos/git-for-windows/git/releases/latest"
+$asset = Invoke-RestMethod -Method Get -Uri $git_url | ForEach-Object assets | Where-Object name -like "*32-bit.exe"
+# download installer
+$installer = "$env:temp\$($asset.name)"
+Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $installer
+# run installer
+$git_install_inf = "<install inf file>"
+$install_args = "/SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /LOADINF=""$git_install_inf"""
+Start-Process -FilePath $installer -ArgumentList $install_args -Wait
+
+## Create detection method. 
+$logfilespath = "C:\logfiles"
+If(!(test-path $logfilespath))
+{
+      New-Item -ItemType Directory -Force -Path $logfilespath
+}
+
+New-Item -ItemType "file" -Path "c:\logfiles\Update-Gitx86-160622.txt"
