74d6dee7ee14ef3c8a37a0f24e2f854b217c5853
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2022-06-23 16:34:10 +0100

    updated

---
+++ Application-Install-Scripts-WIP/Win-Install-DisplayLink-Drivers10-2.ps1
@@ -0,0 +1,75 @@
+<# 
+.SYNOPSIS
+   Install Script for DisplayLink Drivers, Download and install Displaylink drivers version 10.1 M2.
+
+.DESCRIPTION
+   Installs DisplayLink Drivers.
+
+.EXAMPLE
+   PS C:\> .\Win-Install-DisplayLink-Drivers.ps1
+   Save the file to your hard drive with a .PS1 extention and run the file from an elavated PowerShell prompt.
+
+.FUNCTIONALITY
+   PowerShell v1+
+#>
+
+Clear-Host
+Write-Verbose "Setting Arguments" -Verbose
+$StartDTM = (Get-Date)
+
+# Application Details.
+
+$Path = $env:TEMP;
+$Vendor = "Synaptics"
+$Product = "DisplayLink USB Graphics Software for Windows10"
+$PackageName = "DisplayLink-Software-Win-10"
+$Version = "10.2 M2"
+$InstallerType = "exe"
+$Source = "$PackageName" + "." + "$InstallerType"
+$URL = "https://www.synaptics.com/sites/default/files/exe_files/2022-04/DisplayLink%20USB%20Graphics%20Software%20for%20Windows10.2%20M2-EXE.exe"
+$LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
+$UnattendedArgs = '-silent -noreboot -suppressUpToDateInfo -ignoreCompatibility'
+$ProgressPreference = 'SilentlyContinue'
+
+Start-Transcript $LogPS
+
+if( -Not (Test-Path -Path $PackageName ) )
+{
+    New-Item -ItemType directory -Path $PackageName
+}
+
+Set-Location $Path
+
+Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
+If (!(Test-Path -Path $Source)) {
+    Invoke-WebRequest -Uri $url -OutFile $Source
+         }
+        Else {
+            Write-Verbose "File exists. Skipping Download." -Verbose
+         }
+
+Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
+(Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+
+Write-Verbose "Customization" -Verbose
+
+## Pause for 45 seconds to allow install to completed, This can be adjusted as needed.
+Start-Sleep -Seconds 45
+
+## Cleanup Downloads
+Remove-Item $Path\$Source
+
+## Create Detection Method. 
+$logfilespath = "C:\logfiles"
+If(!(test-path $logfilespath))
+{
+      New-Item -ItemType Directory -Force -Path $logfilespath
+}
+
+New-Item -ItemType "file" -Path "c:\logfiles\$PackageName-$Version.txt"
+
+Write-Verbose "Stop logging" -Verbose
+$EndDTM = (Get-Date)
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalSeconds) Seconds" -Verbose
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalMinutes) Minutes" -Verbose
+Stop-Transcript
